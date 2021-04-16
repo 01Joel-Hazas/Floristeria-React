52d282cebae64d6e03d5806113c179b7df8104e5
Joel Hazas <joelhazas@MacBook-Pro-de-Pablo.local>
2021-04-16 12:05:26 +0200

    Listar flores y busqueda

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import FloristeriaForm from "./AppFloristeria";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <h1 className="text-center"> Floristería Dulces Pétalos</h1>
+        <hr />
+        <FloristeriaForm />
+      </body>
     </div>
   );
 }
---
+++ src/AppFloristeria.tsx
@@ -0,0 +1,133 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const herokuUrl = "https://dulces-petalos.herokuapp.com/api/product";
+
+function FloristeriaForm() {
+  const [floresData, setfloresData] = useState<any[]>([]);
+  const [searchFloresData, setSearchFloresData] = useState<any[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
+
+  useEffect(() => {
+    getHerokuDataWithAxios();
+  }, []);
+
+  const getHerokuDataWithAxios = async () => {
+    const response = await axios.get(herokuUrl);
+    setfloresData(response.data);
+  };
+
+  function filterSearchedData(searchData: any) {
+    let searchingFlowers: any[] = [];
+    floresData.map((flower) => {
+      if (
+        flower["name"].toString().toLowerCase().includes(searchData) ||
+        flower["binomialName"].toString().toLowerCase().includes(searchData)
+      ) {
+        searchingFlowers.push(flower);
+        setHasError(false);
+        // @ts-ignore: Object is possibly 'null'.
+        document.getElementById("errorMessage").innerHTML = "";
+        setHasValue(true);
+      }
+    });
+
+    if (searchingFlowers.length === 0) {
+      setHasError(true);
+    }
+
+    setSearchFloresData(searchingFlowers);
+  }
+
+  function printAllFlowers() {
+    let AllFlowers: any = floresData.map((flor) => {
+      return (
+        <div className="col-sm-3">
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src={flor["imgUrl"]}
+              className="mx-auto"
+              alt="..."
+              style={{ width: "200px", height: "200px" }}
+            />
+
+            <div className="card-body">
+              <h5 className="card-title">{flor["name"]}</h5>
+              <p className="card-text">{flor["binomialName"]}</p>
+              <p className="card-text">
+                <small className="text-muted">{flor["price"]}€</small>
+              </p>
+              <a href="#" className="btn btn-primary">
+                DETALLES
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return AllFlowers;
+  }
+
+  function showErrorMessage() {
+    let errorMessage = "No se ha encontrado ningúna flor con ese criterio";
+    // @ts-ignore: Object is possibly 'null'.
+    document.getElementById("errorMessage").innerHTML = errorMessage;
+  }
+
+  function printSearchedFlowers() {
+    if (hasError) {
+      showErrorMessage();
+    } else {
+      let SearchedFlowers: any = searchFloresData.map((flor) => {
+        return (
+          <div className="col-sm-3">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                src={flor["imgUrl"]}
+                className="mx-auto"
+                alt="..."
+                style={{ width: "200px", height: "200px" }}
+              />
+
+              <div className="card-body">
+                <h5 className="card-title">{flor["name"]}</h5>
+                <p className="card-text">{flor["binomialName"]}</p>
+                <p className="card-text">
+                  <small className="text-muted">{flor["price"]}€</small>
+                </p>
+                <a href="#" className="btn btn-primary">
+                  DETALLES
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      });
+      return SearchedFlowers;
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Buscar aqui"
+          onChange={(event) => {
+            filterSearchedData(event.target.value);
+          }}
+        />
+        <div className="input-group-append"></div>
+      </div>
+      <br />
+      <div className="row">
+        <p id="errorMessage"> </p>
+        {hasValue ? printSearchedFlowers() : printAllFlowers()}
+      </div>
+    </div>
+  );
+}
+
+export default FloristeriaForm;
